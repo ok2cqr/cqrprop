852514d5e38c5059cb27b797414ff14630aaa89a
ok2cqr <petr@ok2cqr.com>
2016-08-01 13:52:23 +0200

    change form position with a mouse

--- src/fShowPropForm.pas
+++ src/fShowPropForm.pas
@@ -12,10 +12,17 @@ type
   { TfrmShowPropForm }
 
   TfrmShowPropForm = class(TfrmCommon)
+    procedure FormMouseDown(Sender : TObject; Button : TMouseButton;
+      Shift : TShiftState; X, Y : Integer);
+    procedure FormMouseMove(Sender : TObject; Shift : TShiftState; X,
+      Y : Integer);
+    procedure FormMouseUp(Sender : TObject; Button : TMouseButton;
+      Shift : TShiftState; X, Y : Integer);
     procedure FormShow(Sender: TObject);
     procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
   private
-    { private declarations }
+    OldX, OldY : Integer;
+    FormMoving : Boolean;
   public
     { public declarations }
   end;
@@ -26,6 +33,34 @@ var
 implementation
 
 {$R *.lfm}
+
+procedure TfrmShowPropForm.FormMouseDown(Sender : TObject;
+  Button : TMouseButton; Shift : TShiftState; X, Y : Integer);
+begin
+  if (Button = mbLeft) then
+  begin
+    FormMoving := True;
+    OldX := X;
+    OldY := Y
+  end
+end;
+
+procedure TfrmShowPropForm.FormMouseMove(Sender : TObject; Shift : TShiftState;
+  X, Y : Integer);
+begin
+  if FormMoving then
+  begin
+    Left := Left + (X - OldX);
+    Top  := Top + (Y - OldY)
+  end
+end;
+
+procedure TfrmShowPropForm.FormMouseUp(Sender : TObject; Button : TMouseButton;
+  Shift : TShiftState; X, Y : Integer);
+begin
+  FormMoving := False
+end;
+
 procedure TfrmShowPropForm.FormShow(Sender: TObject);
 begin
   inherited;
