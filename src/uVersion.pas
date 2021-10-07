221de13678c6c3279a231a52fd4d67f182be2ad6
Petr Hlozek <petr@petrhloezek.cz>
2021-10-07 20:54:37 +0200

    version changed to 0.7.0

--- src/uVersion.pas
+++ src/uVersion.pas
@@ -4,12 +4,12 @@ unit uVersion;
 interface
 
 const
-  cVERSION    = '0.0.6 (001)';
+  cVERSION    = '0.0.7 (001)';
   cMAJOR      = 0;
   cMINOR      = 0;
-  cRELEAS     = 6;
+  cRELEAS     = 7;
   cBUILD      = 1;
-  cBUILD_DATE = '2020-08-16';
+  cBUILD_DATE = '2021-10-07';
 
 implementation
 
