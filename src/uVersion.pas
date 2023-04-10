57a3b6c2d7af21fdca01287689a9414e89e6006b
Petr Hlozek <petr@petrhlozek.cz>
2023-04-10 14:47:32 +0200

    version changed to 0.0.8

--- src/uVersion.pas
+++ src/uVersion.pas
@@ -4,12 +4,12 @@ unit uVersion;
 interface
 
 const
-  cVERSION    = '0.0.7 (001)';
+  cVERSION    = '0.0.8 (001)';
   cMAJOR      = 0;
   cMINOR      = 0;
-  cRELEAS     = 7;
+  cRELEAS     = 8;
   cBUILD      = 1;
-  cBUILD_DATE = '2021-10-07';
+  cBUILD_DATE = '2023-04-10';
 
 implementation
 
